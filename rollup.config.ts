15770c0fa5cc6942bd85721c58353b98011e8703
Trezy <tre@trezy.com>
2026-01-03 22:58:22 -0600

    build: remove Typescript redundancies

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,5 +1,4 @@
-// See: https://rollupjs.org/introduction/
-
+// Module imports
 import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
 import nodeResolve from '@rollup/plugin-node-resolve'
